23bd10ca60e6e4eb38e2c114a49a1959696f1a4f
kaung-san-hein <kaungsanhein2019@gmail.com>
2020-05-03 15:35:56 +0630

    02 Modularize Express

---
+++ server/handlers/index.js
@@ -0,0 +1,17 @@
+const notFound = (req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+
+  next(error);
+};
+
+const errors = (err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .json({ err: err.message || "Something went wrong" });
+};
+
+module.exports = {
+  errors,
+  notFound,
+};
--- server/index.js
+++ server/index.js
@@ -1,21 +1,20 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const { notFound, errors } = require("./handlers");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.json({ hello: "world" }));
 
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-
-  next(error);
-});
-
-app.use((err, req, res, next) => {
-  res
-    .status(err.status || 500)
-    .json({ err: err.message || "Something went wrong" });
-});
+app.use(notFound);
+app.use(errors);
 
 app.listen(port, console.log(`Server started on port ${port}`));
